0c632633bb13115342f456ae75ba6d8c132e7930
Ashish Suri <75773071+KnowAashish@users.noreply.github.com>
2024-02-07 20:13:50 -0500

    UART - Transaction Class

---
+++ tb/uart_transaction.sv
@@ -0,0 +1,26 @@
+class transaction;
+	
+	rand bit [`DATA_WIDTH-1:0] din;
+	bit newd;
+	bit rx;		// planning to use urandom for this input
+	bit tx;
+	bit done_tx;
+	bit done_rx;
+	bit [`DATA_WIDTH-1:0] dout;
+	
+	typedef enum bit {send=1'b0, receive=1'b1} oper_type;
+	randc oper_type oper;
+	
+	// Logic for deep copy of the transaction packet
+	function transaction copy();
+		copy.din 	 = this.din;
+		copy.newd 	 = this.newd;
+		copy.rx 	 = this.rx;
+		copy.tx 	 = this.tx;
+		copy.done_tx = this.done_tx;
+		copy.done_rx = this.done_rx;
+		copy.dout 	 = this.dout;
+		copy.oper 	 = this.oper;
+	endfunction
+	
+endclass
